fd0b5afd7fbc430ee0fce3bfd31d21ed8bcb790a
moe li <creeperblin@outlook.com>
2024-10-30 00:22:18 +0800

    Add files via upload

---
+++ pkg/flv_wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(): void;
+export function init_local(a: number): void;
+export function push_data(a: number, b: number): void;
+export function start(a: number): void;
+export function stop(a: number): void;
+export function get_codec(a: number): void;
+export function consume(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
